48bade064aa0467d250c749a2d35763875ec7305
jrduell <jrduell7@gmail.com>
2020-12-28 09:25:01 -0600

    Run tab VC created

---
+++ Treads/treads/Controller/StartRunVC.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  treads
+//
+//  Created by Jacob Duell on 12/24/20.
+//
+
+import UIKit
+
+class StartRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    
+
+}
---
+++ Treads/treads/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  treads
+//
+//  Created by Jacob Duell on 12/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
