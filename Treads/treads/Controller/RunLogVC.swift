1ce357f0b43744e1ff87badee0f8c6241b18056f
jrduell <jrduell7@gmail.com>
2020-12-29 12:24:15 -0600

    create all view controllers, start run, run log, and current run

--- Treads/treads/Controller/RunLogVC.swift
+++ Treads/treads/Controller/RunLogVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class RunLogVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Treads/treads/View/EndRunSlider.swift
@@ -0,0 +1,12 @@
+//
+//  EndRunSlider.swift
+//  treads
+//
+//  Created by Jacob Duell on 12/29/20.
+//
+
+import UIKit
+
+class EndRunSlider: UIView {
+    
+}
