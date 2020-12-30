90ab02e8fffc59392c2efad17d990a33e29e141e
jrduell <jrduell7@gmail.com>
2020-12-29 22:03:42 -0600

    custom slider added, dismisses view upon user releasing all the way right, otherwise move back to beginning

--- Treads/treads/Controller/CurrentRunVC.swift
+++ Treads/treads/Controller/CurrentRunVC.swift
@@ -7,23 +7,49 @@
 
 import UIKit
 
-class CurrentRunVC: UIViewController {
-
-    @IBOutlet weak var timeElapsedLabel: UILabel!
-    @IBOutlet weak var avgPaceLabel: UILabel!
-    @IBOutlet weak var distanceLabel: UILabel!
-    @IBOutlet weak var pauseRunBtn: UIButton!
-    @IBOutlet weak var endRunBtn: UIImageView!
-    @IBOutlet weak var endRunLabel: UILabel!
+class CurrentRunVC: LocationVC {
     
+    @IBOutlet weak var sliderImageView: UIImageView!
+    @IBOutlet weak var swipeBGImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
+        sliderImageView.addGestureRecognizer(swipeGesture)
+        sliderImageView.isUserInteractionEnabled = true
+        swipeGesture.delegate = self as? UIGestureRecognizerDelegate
     }
     
+    @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
+        let minAdjust: CGFloat = 80
+        let maxAdjust: CGFloat = 127.5
+        if let sliderView = sender.view {
+            if sender.state == UIGestureRecognizer.State.began || sender.state == UIGestureRecognizer.State.changed {
+                let translation = sender.translation(in: self.view)
+                if sliderView.center.x >= (swipeBGImageView.center.x - minAdjust) && sliderView.center.x <= (swipeBGImageView.center.x + maxAdjust) {
+                    //update position of slider view image
+                    sliderView.center = CGPoint(x: sliderView.center.x + translation.x, y: sliderView.center.y)
+                } else if sliderView.center.x >= (swipeBGImageView.center.x + maxAdjust) {
+                    //don't let slider get too far right
+                    sliderView.center.x = swipeBGImageView.center.x + maxAdjust
+                } else {
+                    //don't let slider get too far left
+                    sliderView.center.x = swipeBGImageView.center.x - minAdjust
+                }
+                sender.setTranslation(CGPoint.zero, in: self.view)
+            } else if sender.state == UIGestureRecognizer.State.ended && sliderView.center.x < (swipeBGImageView.center.x + maxAdjust) {
+                UIView.animate(withDuration: 0.1) {
+                    //reset slider to left if not all the way to right
+                    sliderView.center.x = self.swipeBGImageView.center.x - minAdjust            }
+            } else if sender.state == UIGestureRecognizer.State.ended && sliderView.center.x >= (swipeBGImageView.center.x + maxAdjust) {
+                //dismiss view if user lets go at full right position
+                //End run code goes here
+                dismiss(animated: true, completion: nil)
+            }
+        }
+    }
     @IBAction func pauseRunButtonTapped(_ sender: Any) {
     }
     
 }
+
